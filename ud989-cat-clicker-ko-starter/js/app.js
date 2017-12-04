c04fc8f7e31fd45006cf0bcba9cb88ea9df5c835
ssdwww <ssdwww@qq.com>
2017-12-05 00:19:42 +0800

    提交

--- ud989-cat-clicker-ko-starter/js/app.js
+++ ud989-cat-clicker-ko-starter/js/app.js
@@ -1,16 +1,42 @@
 
-var Cat = function(){
-    this.Nicknames = ko.observableArray([
-        {name:"Tabtab"},
-        {name:"T-Bone"},
-        {name:"Mr.T"},
-        {name:"Tabutha Tab Tabby Catty Cat"},
-    ])
-    this.clickCount = ko.observable(0);
-    this.Na = ko.observable('Nicknames');
-    this.name = ko.observable('Tabby');
-    this.levelone = ko.observable('Infant');
-    this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+
+
+var initialCats=[
+    { clickCount : 0,
+        name:'Tabby',
+        imgSrc:'img/22252709_010df3379e_z.jpg',
+        nicknames:['Tabtab','T-Bone','Mr. T','Tabitha Tab Tabby Catty Cat'],
+
+    },
+    { clickCount : 0,
+        name:'Tigger',
+        imgSrc:'img/434164568_fea0ad4013_z.jpg',
+        nicknames:['Tigger']
+    },
+    { clickCount : 0,
+        name:'Scaredy',
+        imgSrc:'img/1413379559_412a540d29_z.jpg',
+        nicknames:['Casper']
+
+    },{ clickCount : 0,
+        name:'Shadow',
+        imgSrc:'img/4154543904_6e2428c421_z.jpg',
+        nicknames:['Shooby']
+    },{ clickCount : 0,
+        name:'test',
+        imgSrc:'img/9648464288_2516b35537_z.jpg',
+        nicknames:['Zzzzz']
+    }
+]
+
+
+var Cat = function(data){
+    this.Nicknames = ko.observableArray(data.nicknames)
+    this.clickCount = ko.observable(data.clickCount);
+    this.Na = ko.observable(data.Na);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+
     this.level = ko.computed(function() {
         var click = this.clickCount();
         var title;
@@ -39,17 +65,32 @@ var Cat = function(){
 }
 
 
+
+
+
+
+
+
+
+
+
 var ViewModel = function(){
+   var self = this;
+    this.catList = ko.observableArray([])
+    initialCats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem))
+    })
 
-    this.currentCat = ko.observable(new Cat())
-
+    this.currentCat = ko.observable(this.catList()[0])
     this.incrementCounter = function(){
-        this.clickCount(this.clickCount()+1)
+        self.currentCat().clickCount(self.currentCat().clickCount()+1)
+    }
+
+    this.test = function(clickedCat){
+        self.currentCat(clickedCat)
     }
 
 }
-
-
 
 
 
